a91160910d5be2f0e4127e7da82da2442ee0e1cb
Filip Janiszewski <janiszewski.computing@gmail.com>
2016-03-03 12:16:42 +0100

    Adding longer search patterns

--- Cap 1-3/RunPerformance.cpp
+++ Cap 1-3/RunPerformance.cpp
@@ -28,8 +28,8 @@ void bench(long text_size,const vector<string>& search_patterns)
     {
         for(int i{0};i<3;i++)
         {
-            cout<<"Running "<<get<0>(functions[i])<<", pat: \""<<
-                search_patterns[pat_idx]<<"\", time: ";
+            cout<<"Running "<<get<0>(functions[i])<<", pat length: "<<
+                search_patterns[pat_idx].size()<<", time: ";
             auto time_start=chrono::high_resolution_clock::now();
             long c = get<1>(functions[i])(text,search_patterns[pat_idx]).size();
             auto time_stop=chrono::high_resolution_clock::now();
@@ -44,8 +44,46 @@ int main(int argc,char** argv)
 {
     cout.sync_with_stdio(false);
     bench<text_sample_generator>(50000000,{"kill","house","ban","a","yo","little","a and","walhalla"});
-    bench<fibonacci_text_generator>(35,{"a","abba","abaababaabaababaababaabaababaaba",
-            "baabaab","ab","aab"});
-    bench<thue_morse_text_generator>(50000000,{"abba","abbaabba","a","ab","aabb"});
+    bench<fibonacci_text_generator>(40,{"a","abba","abaababaabaababaababaabaababaaba",
+            "baabaab","ab","aab",
+            "abaababaabaababaababaabaababaababaabaababaabaababaababaabaababaabaababaababa",
+            "abaabaababaababaabaababaababaabaababaabaababaababaabaababaabaababaababaabaababaab\
+            abaabaababaabaababaababaabaababaabaababaababaabaababaababaab",
+            "abaababaabaababaabaababaababaabaababaabaababaababaabaababaababaabaababaabaababaab\
+            abaabaababaababaabaababaabaababaababaabaababaabaababaababaabaababaababaabaababaaba\
+            ababaababaabaababaababaabaababaabaababaababaabaababaabaababaababaabaababaababaabaab\
+            abaabaababaababaabaababaabaababaababaabaababaababaabaababaabaababaababaabaababaabab\
+            aabaababaabaababaababaabaababaabaababaababaabaababaababaabaababaabaababaababaabaaba\
+            baabaababaababaabaababaababaabaababaabaababaababaabaababaababaabaababaabaababaababa\
+            abaababaabaababaababaabaababaababaabaababaabaababaababaabaababaababaabaababaabaabab\
+            aababaabaababaabaababaababaabaababaababaabaababaabaababaababaabaababaabaababaababaa\
+            baababaababaabaababaabaababaababaabaababaababaabaababaabaababaababaabaababaabaababa\
+            ababaabaababaababaabaababaabaababaababaabaababaabaababaababaabaababaababaabaababaab\
+            aababaababaabaababaababaabaababaabaababaababaabaababaabaababaababaabaababaababaabaa\
+            babaabaababaababaabaababaababaabaababaabaababaababaabaababaabaababaababaabaababaaba\
+            baabaababaabaababaababaabaababaabaababaababaabaababaababaabaababaabaababaababaabaab\
+            abaababaabaababaabaababaababaabaababaabaababaababaabaababaababaabaababaabaababaabab\
+            aabaababaababaabaababaabaababaababaabaababaabaababaababaabaababaababaabaababaabaaba\
+            baababaabaababaabaababaababaabaababaababaabaababaabaababaababaabaababaababaabaababa\
+            abaababaab"});
+    bench<thue_morse_text_generator>(50000000,{"abba","abbaabba","a","ab","aabb",
+            "aabbaababbaabbabaabbaababbabaababbaabbabaabbaababbaabbabaababbabaabbaababbaabbaba",
+            "abaababbaabbabaabbaababbaabbabaababbabaabbaababbabaababbaabbabaababbabaabbaababba\
+            abbabaabbaababbabaababbaabbabaabbaababbaabbabaababbabaabbaababbaabbabaabbaababbaba\
+            ababbaabbabaababbabaabbaababbabaababbaabbabaabbaababbaabbabaababbabaabbaababbabaab",
+            "bbabaababbabaabbaababbabaababbaabbabaababbabaabbaababbaabbabaabbaababbabaababbaab\
+            babaabbaababbaabbabaababbabaabbaababbaabbabaabbaababbabaababbaabbabaababbabaabbaab\
+            abbabaababbaabbabaabbaababbaabbabaababbabaabbaababbaabbabaabbaababbabaababbaabbaba\
+            abbaababbaabbabaababbabaabbaababbabaababbaabbabaababbabaabbaababbaabbabaabbaababba\
+            baababbaabbabaababbabaabbaababbabaababbaabbabaabbaababbaabbabaababbabaabbaababbaba\
+            ababbaabbabaababbabaabbaababbaabbabaabbaababbabaababbaabbabaabbaababbaabbabaababba\
+            baabbaababbaabbabaabbaababbabaababbaabbabaababbabaabbaababbabaababbaabbabaabbaabab\
+            baabbabaababbabaabbaababbaabbabaabbaababbabaababbaabbabaabbaababbaabbabaababbabaab\
+            baababbabaababbaabbabaababbabaabbaababbaabbabaabbaababbabaababbaabbabaabbaababbaab\
+            babaababbabaabbaababbaabbabaabbaababbabaababbaabbabaababbabaabbaababbabaababbaabba\
+            baabbaababbaabbabaababbabaabbaababbabaababbaabbabaababbabaabbaababbaabbabaabbaabab\
+            babaababbaabbabaababbabaabbaababbabaababbaabbabaabbaababbaabbabaababbabaabbaababba\
+            abbabaabbaababbabaababbaabbabaabbaababbaabbabaababbabaabbaababbabaababbaabbabaabab\
+            babaabbaababb"});
     return 0;
 }
